b07fd1dc3c6174232f615d1ef5d3a3bea2e8f63a
kimpham54 <kimpham54@gmail.com>
2018-03-21 19:12:40 -0400

    cleanup code

--- geocoder.py
+++ geocoder.py
@@ -5,25 +5,23 @@ from geopy.geocoders import Nominatim, GoogleV3
 
 inputfile=str(sys.argv[1])
 namecolumn=str(sys.argv[2])
-# your command will look something like 'python geocoder-singlecolumn.py census-historic-population-borough.csv Area_Name'
+# your command will look something like 'python2/python3 geocoder-singlecolumn.py census-historic-population-borough.csv Area_Name'
 
 def main():
   io = pandas.read_csv(inputfile, index_col=None, header=0, sep=",")
 
   def get_latitude(x):
-    print(x)
+    # print(x)
     if x is None:
-      x = '24 aldridge ave. m4c 3w4 toronto canada'
-      print(x, 'lat not found')
+      return x
     else:
       return x.latitude
-    # how do i assign an int or any value to x if it's none?
+    # how do i assign an int or any value to x if it's none? how do i get the initial input x?
 
   def get_longitude(x):
-    print(x)
+    # print(x)
     if x is None:
-      x = '24 aldridge ave. m4c 3w4 toronto canada'
-      print(x, 'long not found')
+      return x
     else:
       return x.longitude
 
@@ -31,18 +29,12 @@ def main():
   # geolocator = GoogleV3(timeout=5)
   # uncomment the geolocator you want to use
   # change the timeout value if you get a timeout error, for instance, geolocator = Nominatim(timeout=60)
+  # io[namecolumn] is a series or object dtype, you apply action to an entire series not a single value
   geolocate_column = io[namecolumn].apply(geolocator.geocode)
   io['latitude'] = geolocate_column.apply(get_latitude)
   io['longitude'] = geolocate_column.apply(get_longitude)
   print(io['longitude'])
   # print(type(io['latitude']))
-
-
-  # if io['latitude'] is None:
-  #   io['latitude'] =  0.000
-
-  # if io['longitude'] is None:
-  #   io['longitude'] = 0.000
 
   io.to_csv('geocoding-output.csv')
 
